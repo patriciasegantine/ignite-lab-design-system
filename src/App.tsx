efdd4558accaf830a8f879669b2131d06c8508b9
Patricia Segantine <pnsegantine@gmail.com>
2022-10-14 14:14:49 +0100

    add theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import {GlobalStyle} from "./global.styles";
+import {ThemeProvider} from "styled-components";
+import {defaultTheme} from "./theme/defaultTheme";
 
 export function App() {
-    return (
-        <>
-            <GlobalStyle/>
-            <div>Hello Word</div>
-        </>
-    );
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <div>Hello Word</div>
+    </ThemeProvider>
+  );
+  
 }
---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+  white: '#FFF',
+  
+  'gray-100': '#E1E1E6',
+  'gray-300': '#7C7C7A',
+  'gray-500': '#7C7C8A',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+  
+  'orange-500': '#996515',
+  'orange-900': '#B45309',
+}
